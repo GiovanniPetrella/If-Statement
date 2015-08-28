5e71951c3b7544d605acd9dac6ed177ad15b3723
GiovanniPetrella <tangolikeamango1@gmail.com>
2015-08-27 19:24:07 -0700

    If Statement
    If Statement

---
+++ src/apples.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class apples {
+	public static void main(String args[]){
+		Scanner bucky = new Scanner(System.in);
+		int test = 6;
+			
+		if (test == 9)
+		{
+			System.out.println("yes");
+		}else{
+			System.out.println("this is else");
+		}
+	}
+}
